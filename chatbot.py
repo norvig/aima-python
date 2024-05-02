f54c90d20da01d932b2d2f47da2fbd6bae2074e6
María Paula Rodríguez Muñoz <A01254734@tec.mx>
2024-05-02 00:27:53 +0000

    Co-authored-by: René  <rene-calz@users.noreply.github.com> Co-authored-by: Gabriela Lujan <GabrielaLujanL@users.noreply.github.com>

--- chatbot.py
+++ chatbot.py
@@ -44,6 +44,24 @@ clauses.append(expr("Enfermo(x, Taxicodermia) ==> Tratamiento(x, NoMedicamentos)
 clauses.append(expr("(Sintoma(x, Picor) & Sintoma(x, Hinchazon) & Sintoma(x, Enrojecimiento) & Sintoma(x, RonchaRojas)) ==> Enfermo(x, Urticaria)"))
 clauses.append(expr("Enfermo(x, Urticaria) ==> Tratamiento(x, Corticoides)"))
 clauses.append(expr("Enfermo(x, Urticaria) ==> Tratamiento(x, Antihistaminico)"))
+clauses.append(expr("Sintoma(x, Fiebre) ==> Tratamiento(x, Paracetamol)"))
+clauses.append(expr("Sintoma(x, FiebreAlta) ==> Tratamiento(x, Paracetamol)"))
+
+#Maria Paula
+#clauses.append(expr(""))
+clauses.append(expr("Sintoma(x, PuntosBlancos) ==> Enfermo(x, Acne)"))
+clauses.append(expr("(Sintoma(x, PuntosBlancos) & Sintoma(x, PuntosNegros) & Sintoma(x, Papulas)) ==> Enfermo(x, Acne)"))
+clauses.append(expr("Sintoma(x, Papulas) ==> Enfermo(x, Acne)"))
+clauses.append(expr("(Sintoma(x, PuntosBlancos) & Sintoma(x, Papulas) & Sintoma(x, Seborrea)) ==> Enfermo(x, Acne)"))
+clauses.append(expr("(Sintoma(x, PuntosBlancos) & Sintoma(x, PuntosNegros) & Sintoma(x, Papulas) & Sintoma(x, Seborrea)) ==> Enfermo(x, Acne)"))
+clauses.append(expr("Enfermo(x, Acne) ==> Tratamiento(Clindamicina)"))
+clauses.append(expr("Enfermo(x, Acne) ==> Tratamiento(x, GelPeroxidoBenzoilo)"))
+clauses.append(expr("Enfermo(x, Acne) ==> Tretinoina"))
+clauses.append(expr("(Sintoma(x, Vesiculas) & Sintoma(x, DolorBoca) & Sintoma(x, Fiebre)) ==> Enfermo(x, Herpes)"))
+clauses.append(expr("(Sintoma(x, Vesiculas) & Sintoma(x, Fiebre)) ==> Enfermo(x, Herpes)"))
+clauses.append(expr("(Sintoma(x, Vesiculas) & Sintoma(x, DolorBoca)) ==> Enfermo(x, Herpes)"))
+
+# Gabriela Lujan
 
 
 doctor_kb = FolKB(clauses)
@@ -55,37 +73,87 @@ name = input("¿Cual es tu nombre? ")
 if (input("¿Tienes enrojecimiento en la piel? ").lower() == 's'): 
     doctor_kb.tell(expr("Sintoma({}, Enrojecimiento)".format(name)))
 
+dec = fol_fc_ask(doctor_kb, expr("Sintoma({}, Enrojecimiento)".format(name)))
+dec = list(dec)
 
+if len(dec) > 0: 
 
-if (input("¿Presenta ojos irritados? ").lower() == 's'): 
-    doctor_kb.tell(expr("Sintoma({}, OjosIrritados)".format(name))) 
+    if (input("¿Presenta ojos irritados? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, OjosIrritados)".format(name))) 
 
-if (input("¿Presenta ojos lacrimosos? ").lower() == 's'): 
-    doctor_kb.tell(expr("Sintoma({}, OjosLacrimosos)".format(name))) 
+    if (input("¿Presenta ojos lacrimosos? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, OjosLacrimosos)".format(name))) 
 
-if (input("¿Presenta ojos enrojecidos? ").lower() == 's'): 
-    doctor_kb.tell(expr("Sintoma({}, OjosEnrojecidos)".format(name))) 
+    if (input("¿Presenta ojos enrojecidos? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, OjosEnrojecidos)".format(name))) 
 
-if (input("¿Presenta vision borrosa? ").lower() == 's'): 
-    doctor_kb.tell(expr("Sintoma({}, VisionBorrosa)".format(name))) 
+    if (input("¿Presenta vision borrosa? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, VisionBorrosa)".format(name)))
 
+    if (input("¿Presenta piel inflamada? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, PielInflamada)".format(name)))
+
+    if (input("¿Presenta piel sensible? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, PielSensible)".format(name)))
+
+    if (input("¿Presenta piel graso? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, PielGraso)".format(name)))
+
+    if (input("¿Presenta Brotes en la piel? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, Brotes)".format(name)))
+
+    if (input("¿Presenta ardor en la piel? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, Ardor)".format(name)))
+
+    if (input("¿Presenta poros dilatados? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, PorosDilatados)".format(name)))
+
+    if (input("¿Presenta bultos? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, Bultos)".format(name))) 
+
+#----------------------------------
 if (input("¿Presenta Fiebre? ").lower() == 's'): 
     doctor_kb.tell(expr("Sintoma({}, Fiebre)".format(name))) 
 
+dec = fol_fc_ask(doctor_kb, expr("Sintoma({}, Fiebre)".format(name)))
+dec = list(dec)
+
+if len(dec) > 0: 
+    if (input("¿Presentan sus conocidos Fiebre Alta? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, FiebreAlta)".format(name)))
+
+#----------------------------------
 if (input("¿Presenta Picor? ").lower() == 's'): 
     doctor_kb.tell(expr("Sintoma({}, Picor)".format(name)))
 
+dec = fol_fc_ask(doctor_kb, expr("Sintoma({}, Picor)".format(name)))
+dec = list(dec)
+
+if len(dec) > 0: 
+    if (input("¿Presentan sus conocidos Picor? ").lower() == 's'): 
+        doctor_kb.tell(expr("Sintoma({}, PicorConocidos)".format(name)))
+
+    if (input("¿Tienes surcos en la piel? ").lower() == 's'):
+        doctor_kb.tell(expr("Sintoma({}, Surcos)".format(name)))
+
+#----------------------
 if (input("¿Presenta hinchazon? ").lower() == 's'): 
     doctor_kb.tell(expr("Sintoma({}, Hinchazon)".format(name))) 
 
 if (input("¿Presenta telaranas en la piel? ").lower() == 's'): 
     doctor_kb.tell(expr("Sintoma({}, Telarana)".format(name))) 
 
+if (input("¿Presenta hinchazon en la piel? ").lower() == 's'): 
+    doctor_kb.tell(expr("Sintoma({}, Hinchazon)".format(name)))
+
+if (input("¿Presenta Ronchas Rojas en la piel? ").lower() == 's'): 
+    doctor_kb.tell(expr("Sintoma({}, RonchasRojas)".format(name)))
+
 
 ### Tratamiento 
-print("\n Este es el tratamiento a llevar \n")
+print("\nEste es el tratamiento a llevar: ")
 answer_trat = fol_fc_ask(doctor_kb, expr("Tratamiento({}, x)".format(name)))
-answer_trat = list(answer)
+answer_trat = list(answer_trat)
 
 if len(answer_trat) > 0: 
     for sintoms in answer_trat: 
@@ -95,7 +163,7 @@ else:
     print("No hay tratamiento, te vas a morir muajaja")
 
 ### Recomendaciones
-print("\n Estas son las recomendaciones a llevar \n")
+print("\nEstas son las recomendaciones a llevar: ")
 answer_rec = fol_fc_ask(doctor_kb, expr("Recomendacion({}, x)".format(name)))
 answer_rec = list(answer_rec)
 
@@ -107,7 +175,7 @@ else:
     print("No hay recomendaciones")
 
 ### Diagnostico 
-print("\n Este es el diagnostico \n")
+print("\nEste es el diagnostico: ")
 answer_di = fol_fc_ask(doctor_kb, expr("Enfermo({}, x)".format(name)))
 answer_di = list(answer_di)
 
